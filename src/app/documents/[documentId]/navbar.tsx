aa679685ca2c0112d04f95eb443abed2eca546cf
Yogesh Ahire <yogeshpahire05@gmail,com>
2025-11-07 14:58:57 +0530

    navbar funtinalities

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -37,17 +37,67 @@ import {
 
 import { DocumentInput } from "./document-input";
 import { useState } from "react";
+import { useEditorStore } from "@/store/use-editor-store";
 
 
 export const Navbar = () => {
-    /*table ghatgpt fuctinalities */
+
+    const { editor } = useEditorStore();
+
+    /*table insert fuctinalities */
     const [rows, setRows] = useState(0);
     const [cols, setCols] = useState(0);
 
-    const handleInsertTable = (r: number, c: number) => {
-        console.log(`Insert table: ${r} x ${c}`);
-        // Later: integrate with editor insertTable(r, c)
+    const insertTable = ({rows, cols}: {rows: number, cols: number}) =>{
+        editor
+        ?.chain()
+        .focus()
+        .insertTable({ rows, cols, withHeaderRow: false})
+        .run()
     };
+
+    const handleInsertTable = (r: number, c: number) => {
+        insertTable({rows:r, cols:c});
+    };
+
+    const onDownload = (blob: Blob, filename: string) => {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        a.click();
+    };
+
+    const onSaveJSON = () =>{
+        if(!editor) return;
+
+        const content = editor.getJSON();
+        const blob = new Blob([JSON.stringify(content)], {
+            type: "application/json",
+        });
+        onDownload(blob, `document.json`) //TODO : Use document name
+    }
+    
+    const onSaveHTML = () =>{
+        if(!editor) return;
+
+        const content = editor.getHTML();
+        const blob = new Blob([content], {
+            type: "text/html",
+        });
+        onDownload(blob, `document.html`) //TODO : Use document name
+    }
+    
+    const onSaveText = () =>{
+        if(!editor) return;
+
+        const content = editor.getText();
+        const blob = new Blob([content], {
+            type: "text/plain",
+        });
+        onDownload(blob, `document.txt`) //TODO : Use document name
+    }
+
 
 
     return (
@@ -72,10 +122,10 @@ export const Navbar = () => {
                                             Save
                                         </MenubarSubTrigger>
                                         <MenubarSubContent>
-                                            <MenubarItem><FileJsonIcon className="size-4 mr-2" /> JSON</MenubarItem>
-                                            <MenubarItem><GlobeIcon className="size-4 mr-2" /> HTML</MenubarItem>
-                                            <MenubarItem><BsFilePdf className="size-4 mr-2" /> PDF</MenubarItem>
-                                            <MenubarItem><FileTextIcon className="size-4 mr-2" /> Text</MenubarItem>
+                                            <MenubarItem onClick={onSaveJSON}><FileJsonIcon className="size-4 mr-2" /> JSON</MenubarItem>
+                                            <MenubarItem onClick={onSaveHTML}><GlobeIcon className="size-4 mr-2" /> HTML</MenubarItem>
+                                            <MenubarItem onClick={()=> window.print()}><BsFilePdf className="size-4 mr-2" /> PDF</MenubarItem>
+                                            <MenubarItem onClick={onSaveText}><FileTextIcon className="size-4 mr-2" /> Text</MenubarItem>
                                         </MenubarSubContent>
                                     </MenubarSub>
                                     <MenubarItem>
@@ -105,11 +155,11 @@ export const Navbar = () => {
                                     Edit
                                 </MenubarTrigger>
                                 <MenubarContent>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={() => editor?.chain().focus().undo().run()}>
                                         <Undo2Icon className="size-4 mr-2" />
                                         Undo <MenubarShortcut> ctrl + z</MenubarShortcut>
                                     </MenubarItem>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={() => editor?.chain().focus().redo().run()}>
                                         <Redo2Icon className="size-4 mr-2" />
                                         Redo <MenubarShortcut> ctrl + y</MenubarShortcut>
                                     </MenubarItem>
@@ -168,28 +218,28 @@ export const Navbar = () => {
                                             Text
                                         </MenubarSubTrigger>
                                         <MenubarSubContent>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={()=> editor?.chain().focus().toggleBold().run()}>
                                                 <BoldIcon className="size-4 mr-2" /> Bold
                                                 <MenubarShortcut>Ctrl + B</MenubarShortcut>
                                             </MenubarItem>
 
-                                            <MenubarItem>
+                                            <MenubarItem onClick={()=> editor?.chain().focus().toggleItalic().run()}>
                                                 <ItalicIcon className="size-4 mr-2" /> Italic
                                                 <MenubarShortcut>Ctrl + I</MenubarShortcut>
                                             </MenubarItem>
 
-                                            <MenubarItem>
+                                            <MenubarItem onClick={()=> editor?.chain().focus().toggleUnderline().run()}>
                                                 <UnderlineIcon className="size-4 mr-2" /> Underline
                                                 <MenubarShortcut>Ctrl + U</MenubarShortcut>
                                             </MenubarItem>
 
-                                            <MenubarItem>
+                                            <MenubarItem onClick={()=> editor?.chain().focus().toggleStrike().run()}>
                                                 <StrikethroughIcon className="size-4 mr-2" /> <span>Strikethroug &nbsp;&nbsp;</span>
                                                 <MenubarShortcut>Ctrl + Shift + X</MenubarShortcut>
                                             </MenubarItem>
                                         </MenubarSubContent>
                                     </MenubarSub>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={()=> editor?.chain().focus().unsetAllMarks().run()}>
                                         <RemoveFormattingIcon className="size-4 rm-2"/>
                                         Clear Formating
                                     </MenubarItem>
